c9e2c369b0fef2798b5b3d044c089e336481b939
aqib javed <aqibbutt3078@gmail.com>
2025-01-11 09:42:38 +0000

    Is tree a valid BST using python3

--- bst/IsValidBST.py
+++ bst/IsValidBST.py
@@ -1,16 +1,18 @@
-import sys
 from typing import Optional
 from bst.TreeNode import TreeNode
 
 
 class IsValidBst:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def solve(root, low, high) -> bool:
-            if root is None:
-                return True
-            if low >= root.val or high <= root.val:
-                return False
+        return self.is_valid_bst_recur(root, float("-inf"), float("inf"))
 
-            return solve(root.left, low, root.val) and solve(root.right, root.val, high)
+    def is_valid_bst_recur(self, root: TreeNode, low: float, high: float) -> bool:
+        if not root:
+            return True
 
-        return solve(root, float('-inf'), float('inf'))
+        if low >= root.val or high <= root.val:
+            return False
+
+        return self.is_valid_bst_recur(
+            root.left, low, root.val
+        ) and self.is_valid_bst_recur(root.right, root.val, high)
