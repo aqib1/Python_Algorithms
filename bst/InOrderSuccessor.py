834b15e207af6bc7718bcce6cc8876aae0f0534d
aqib javed <aqibbutt3078@gmail.com>
2025-01-11 10:29:27 +0000

    In Order successor using iterative approach

--- bst/InOrderSuccessor.py
+++ bst/InOrderSuccessor.py
@@ -15,3 +15,17 @@ class Solution:
             return self.successor(node.right, p, successor)
         return self.successor(node.left, p, node)
 
+    # time complexity OLog(n) or O(h)
+    # space complexity O(1)
+    def successor_iterative(self, node: TreeNode, p: TreeNode) -> Optional[TreeNode]:
+        successor = None
+
+        while node:
+            if p.val >= node.val:
+                node = node.right
+            else:
+                successor = node
+                node = node.left
+
+        return successor
+
