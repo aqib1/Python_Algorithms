233ec45913a99a309638da055b1630130e126d27
aqib javed <aqibbutt3078@gmail.com>
2025-06-12 01:02:03 +0100

    Using pgmpy Liberary

---
+++ bayes_net/play_tennis_probability_using_pgmpy.py
@@ -0,0 +1,8 @@
+from IPython.display import Image
+from pgmpy.utils import get_example_model
+
+if __name__ == '__main__':
+    discrete_bn = get_example_model('asia')
+    viz = discrete_bn.to_graphviz()
+    viz.draw('asia.png', prog='neato')
+    Image('asia.png')
