2c355a1cbe550e3bbf486f5b9486e1ba0f53810d
aqib javed <aqibbutt3078@gmail.com>
2025-08-13 01:02:03 +0100

    Pivot table count agg function

--- AnacondaProjects/pivot_table_example.py
+++ AnacondaProjects/pivot_table_example.py
@@ -2,5 +2,15 @@ import pandas as pd
 
 if __name__ == '__main__':
     revenue = pd.read_csv('salesmen.csv')
-    revenuePivotTable = revenue.pivot_table(values='Revenue', index='Salesman')
-    print(revenuePivotTable)
+    ## By default, the aggregation function will be mean.
+    revenueMeanPivotTable = revenue.pivot_table(values='Revenue', index='Salesman')
+    revenueMeanPivotTable.columns = ['Revenue Mean']
+    print(revenueMeanPivotTable)
+
+    revenueCountPivotTable = revenue.pivot_table(
+        values='Revenue',
+        index='Salesman',
+        aggfunc='count'
+    )
+    revenueCountPivotTable.columns = ['Revenue Count']
+    print(revenueCountPivotTable)
