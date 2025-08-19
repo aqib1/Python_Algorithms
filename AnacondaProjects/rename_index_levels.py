8a1726630953476ca4eac73149d64f16624a3cb2
aqib javed <aqibbutt3078@gmail.com>
2025-08-19 01:02:03 +0100

    Rename multi index column name

--- AnacondaProjects/rename_index_levels.py
+++ AnacondaProjects/rename_index_levels.py
@@ -11,3 +11,15 @@ if __name__ == '__main__':
 
     df.index.set_names(names=['Sale Date'], inplace=True)
     print(df.head())
+
+
+    quarters = pd.read_csv(
+        'quarters.csv',
+        parse_dates=['Date'],
+        date_format='%m/%d/%Y',
+        index_col=['Date', 'Salesman']
+    ).sort_index()
+    print(quarters.head())
+    quarters.index.set_names(names="Sale Date", level=0, inplace=True)
+    quarters.index.set_names(names="Name", level=1, inplace=True)
+    print(quarters.head())
