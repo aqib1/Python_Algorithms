43009a541eaede1efd01fd0f0f7d75ad0ab566b7
aqib javed <aqibbutt3078@gmail.com>
2025-09-17 13:43:47 +0100

    Numpy Operations

---
+++ python_datascience/numpy/NumpyOperations.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+if __name__ == '__main__':
+    arr = np.arange(0, 11)
+    print(arr)
+
+    print(arr + arr)
+    print(arr - arr)
+    print(arr * arr)
+#    print(arr / arr)
+#    print(arr % arr)
+    print(arr + 1)
+    print(arr - 1)
+    print(arr ** 2)
+    print(np.sqrt(arr))
+    print(np.square(arr))
+    print(np.max(arr))
+    print(np.min(arr))
+    print(np.sin(arr))
+    print(np.log(arr))
+    print(np.cosh(arr))
