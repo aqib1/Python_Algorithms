46b397fe4c1d53eb774e64cbb59867d527d9b7b0
aqib javed <aqibbutt3078@gmail.com>
2025-09-21 21:14:25 +0100

    Panda series

---
+++ python_datascience/pandas_intro/PandasSeries.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+    labels = ['a', 'b', 'c']
+    data = [1, 2, 3]
+    arr = np.array(data)
+    d = {'a': 11, 'b': 12, 'c': 13}
+
+    series = pd.Series(data)
+    print(series)
+
+    series_labels = pd.Series(data, index=labels)
+    print(series_labels)
+
+    ## series from dictionary
+    series_from_dic = pd.Series(d)
+    print(series_from_dic)
+
+    ## Pandas series can hold functions too
+    function_series = pd.Series(data = [sum, print, len])
+    print(function_series)
+
+    s1 = pd.Series(
+        [1, 2, 3, 4],
+        ['UK', 'Netherlands', 'Germany', 'Hungary']
+    )
+
+    s2 = pd.Series(
+        [5, 1, 6, 10],
+        ['UK', 'Netherlands', 'China', 'Hungary']
+    )
+
+    ## China and Germany will have NaN
+    print(s1 + s2)
