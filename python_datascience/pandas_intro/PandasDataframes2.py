0f8a6881f473ad9be248ae6999479eaf419603a5
aqib javed <aqibbutt3078@gmail.com>
2025-09-25 01:22:15 +0100

    Pandas Dataframes II

--- python_datascience/pandas_intro/PandasDataframes2.py
+++ python_datascience/pandas_intro/PandasDataframes2.py
@@ -12,4 +12,16 @@ if __name__ == '__main__':
     print(df['W'] > 0)
 
     print('*' * 50)
+
+    ## python and operator will not work here as it work only with single boolean values
+    ## & and | operators will be used for a series of values
     print(df[(df['W'] > 0) & (df['X'] > 0)][['X', 'Y', 'SUM']])
+    print('*' * 50)
+    print(df[(df['W'] > 0) | (df['X'] > 0)][['X', 'Y', 'SUM']])
+    # df.reset_index(inplace=True)
+    print('*' * 50)
+
+    states = 'SA CA ED BN MK'.split()
+    df['STATES'] = states
+    df.set_index('STATES', inplace=True)
+    print(df)
