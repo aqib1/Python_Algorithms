554fc30004536e1a6c0ec2b6878dc9e3921b9a85
aqib javed <aqibbutt3078@gmail.com>
2025-11-04 11:03:23 +0000

    Python operations

---
+++ python_datascience/Operations.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+    print("Mathematical Operations")
+    print(1 + 1)
+    print(1 * 3)
+    print(1 / 2)
+    print(5 - 1)
+    print(2 ** 4) ## 2 Power 4
+    print(2 + 3 * 5 + 5) ## DMAS rule will be applied
+    print(4 % 2) ## Reminder will be zero
+
+    number_two = 2
+    print(number_two)
+
+    first_name = 'Aqib'
+    print("My first name is {}".format(first_name))
+
+    last_name = "Javed"
+    print(last_name[3].upper())
+
+    long_sentence = "Hi this is a sentence and its very long"
+    print(long_sentence[2:16])
+    print(long_sentence[:21])
+    print(long_sentence[16:])
+    print(long_sentence[::-2]) ## Reverse string
+
+    nest = [1,2,3,[4,5,['target', 'extended']]]
+    print(nest[3][2][1])
+
