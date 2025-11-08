d91b561fc26f01d8bf8858bd8842399b64bc5906
aqib javed <aqibbutt3078@gmail.com>
2025-11-08 15:35:17 +0000

    IsEvent method

--- python_datascience/Conditions.py
+++ python_datascience/Conditions.py
@@ -5,7 +5,17 @@ class Conditions:
         else:
             return False
 
+    def print(self, number: int):
+        if number == 1:
+            print("First")
+        elif number == 2:
+            print("Second")
+        elif number == 3:
+            print("Third")
+        else:
+            print("Unknown")
 
 if __name__ == '__main__':
     condition = Conditions()
     print(condition.is_event(12))
+    condition.print(1)
