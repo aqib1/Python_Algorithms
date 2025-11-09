008347371e06e4be443941c2035824f8b49ca40f
aqib javed <aqibbutt3078@gmail.com>
2025-11-09 15:48:17 +0000

    Loops

--- python_datascience/Conditions.py
+++ python_datascience/Conditions.py
@@ -19,3 +19,18 @@ if __name__ == '__main__':
     condition = Conditions()
     print(condition.is_event(12))
     condition.print(1)
+
+    numbers = [1, 2, 3, 4, 5, 6, 7]
+
+    ## Enhanced for loop
+    for item in numbers:
+        print('Item is: {}'.format(item))
+
+    print('* ' * 20)
+
+    ## While loop
+    i = 1
+
+    while i <= 5:
+        print('I is: {}'.format(i))
+        i = i + 1
