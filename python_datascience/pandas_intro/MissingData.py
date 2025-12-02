0d1d562f8c1b5306fdf900279eed39c364bb0bb5
aqib javed <aqibbutt3078@gmail.com>
2025-12-02 09:35:57 +0000

    Fill na values

--- python_datascience/pandas_intro/MissingData.py
+++ python_datascience/pandas_intro/MissingData.py
@@ -4,7 +4,7 @@ import numpy as np
 if __name__ == '__main__':
     data = {
         'A': [1, 2, 3, np.nan],
-        'B': [4, np.nan, 5, 6],
+        'B': [4, np.nan, 5, np.nan],
         'C': [8, 9, 0, 9]
     }
 
@@ -16,3 +16,13 @@ if __name__ == '__main__':
 
     print('*' * 50)
     print(df_data.dropna(axis=1))  ### Drop columns which have nan values
+
+    print(df_data.dropna(thresh=2))  ### Drop rows with a threshold of 2 nan values
+
+    print('*' * 50)
+    ## Fill missing values
+    df_data.fillna(value=0.0, inplace=True)
+    print(df_data)
+
+    print('*' * 50)
+    print(df_data['A'].mean())
